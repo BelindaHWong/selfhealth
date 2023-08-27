834397831915e557288eff18d08593d8f21dba69
Belinda Wong <bwon6110@uni.sydney.edu.au>
2023-08-28 00:27:10 +1000

    Added experiment recommendation

--- SelfHealth/Experiment Setup/ExperimentRecommendation.swift
+++ SelfHealth/Experiment Setup/ExperimentRecommendation.swift
@@ -31,8 +31,16 @@ let averageMins: [HealthData] = [
     
 ]
 
-struct LocationsChart: View {
+struct StepsChart: View {
     var body: some View {
+        Text("Steps")
+            .font(.title2)
+            .fontWeight(.semibold)
+        
+        Text("Your daily average of 9,212 steps across 18 months is less than the WHO guideline of 10,000 steps per day.")
+            .font(.footnote)
+//            .foregroundStyle(.gray)
+        let recValue = 10000
         Chart {
             ForEach(seriesData, id: \.type) { series in
                 ForEach(series.data, id: \.weekday) {
@@ -41,35 +49,90 @@ struct LocationsChart: View {
                         y: .value("Sales", $0.sales)
                     )
                 }
+//                .foregroundStyle(.blue.opacity(0.25))
                 .foregroundStyle(by: .value("City", series.type))
                 .symbol(by: .value("City", series.type))
                 .interpolationMethod(.catmullRom)
 
             }
-        }.chartLegend(position: .bottom, alignment: .center, spacing: 7)
+            RuleMark(
+                y: .value("Recommend", recValue)
+            )
+            .lineStyle(StrokeStyle(lineWidth: 3)).foregroundStyle(.orange)
+//            .annotation(position: .top, alignment: .trailing) {
+//                Text("Recommended: \(recValue, format: .number)")
+//                    .font(.headline)
+//                    .foregroundStyle(.orange)
+//            
+//            }
+        }
+    
+        .chartLegend(position: .bottom, alignment: .center, spacing: 7)
     }
 
     let seriesData = [
         (
-            type: "Standing", data: [
+            type: "Active Minutes", data: [
+                (weekday: "Mon", sales: 8423),
+                (weekday: "Tue", sales: 6843),
+                (weekday: "Wed", sales: 12282),
+                (weekday: "Thu", sales: 10221),
+                (weekday: "Fri", sales: 16721),
+                (weekday: "Sat", sales: 7877),
+                (weekday: "Sun", sales: 6400)
+            ]
+        )
+    ]
+}
+
+struct LocationsChart: View {
+    var body: some View {
+        Text("Active Minutes")
+            .font(.title2)
+            .fontWeight(.semibold)
+        
+        Text("Your daily average of 80 minutes across 18 months is more than the WHO guideline of 60 minutes per day.")
+            .font(.footnote)
+//            .foregroundStyle(.gray)
+        let recValue = 60
+        Chart {
+            ForEach(seriesData, id: \.type) { series in
+                ForEach(series.data, id: \.weekday) {
+                    LineMark(
+                        x: .value("Weekday", $0.weekday),
+                        y: .value("Sales", $0.sales)
+                    )
+                }
+                .foregroundStyle(.blue.opacity(0.25))
+                .foregroundStyle(by: .value("City", series.type))
+                .symbol(by: .value("City", series.type))
+                .interpolationMethod(.catmullRom)
+
+            }
+            RuleMark(
+                y: .value("Recommend", recValue)
+            )
+            .lineStyle(StrokeStyle(lineWidth: 3)).foregroundStyle(.orange)
+            .annotation(position: .top, alignment: .trailing) {
+                Text("Recommended: \(recValue, format: .number)")
+                    .font(.headline)
+                    .foregroundStyle(.orange)
+            
+            }
+        }
+        .chartLegend(position: .bottom, alignment: .center, spacing: 7)
+    }
+
+    let seriesData = [
+        (
+            type: "Active Minutes", data: [
                 (weekday: "Mon", sales: 54),
                 (weekday: "Tue", sales: 42),
                 (weekday: "Wed", sales: 88),
-                (weekday: "Thu", sales: 49),
-                (weekday: "Fri", sales: 42),
-                (weekday: "Sat", sales: 125),
+                (weekday: "Thu", sales: 79),
+                (weekday: "Fri", sales: 92),
+                (weekday: "Sat", sales: 65),
                 (weekday: "Sun", sales: 67)
-            ]
-        ),
-        (
-            type: "Active", data: [
-                (weekday: "Mon", sales: 81),
-                (weekday: "Tue", sales: 90),
-                (weekday: "Wed", sales: 52),
-                (weekday: "Thu", sales: 72),
-                (weekday: "Fri", sales: 84),
-                (weekday: "Sat", sales: 84),
-                (weekday: "Sun", sales: 137)
             ]
         )
     ]
@@ -80,39 +143,146 @@ func date(year: Int, month: Int, day: Int = 1) -> Date {
 }
 //['2023-03-31', '2023-04-03', '2023-05-08', '2023-05-18', '2023-06-12', '2023-06-19', '2023-07-01', '2023-07-02', '2023-07-03', '2023-07-04', '2023-07-31']
 
+struct DataCompleteness: View {
+    var body: some View {
+        Text("Data Completeness")
+            .font(.title2)
+            .fontWeight(.semibold)
+        
+        Text("How many days of wearable data do you have?")
+            .font(.footnote)
+            .foregroundStyle(.gray)
+        ZStack {
+            Chart(adherenceDays, id: \.name) { element in
+                SectorMark(
+                    angle: .value("Days", element.days),
+                    innerRadius: .ratio(0.618),
+                    angularInset: 1.5
+                )
+                .cornerRadius(4)
+                .foregroundStyle(by: .value("Name", element.name))
+            }
+            .frame(height: 300)
+            .chartXAxis(.hidden)
+            .chartLegend(position: .bottom, alignment: .center, spacing: 7)
+            VStack {
+                Text("98%")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("of days have data")
+                    .font(.subheadline)
+                    .foregroundStyle(.gray)
+            }
+        }
+        .padding()
+    }
+}
+
 struct ExperimentRecommendation: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Data Completeness")
+        VStack(alignment: .leading, spacing: 10) {
+            
+            Text("Select a preferred exercise")
                 .font(.title2)
                 .fontWeight(.semibold)
-            
-            Text("How many days of wearable data do you have?")
-                .font(.footnote)
-                .foregroundStyle(.gray)
-            ZStack {
-                Chart(adherenceDays, id: \.name) { element in
-                    SectorMark(
-                        angle: .value("Days", element.days),
-                        innerRadius: .ratio(0.618),
-                        angularInset: 1.5
-                    )
-                    .cornerRadius(4)
-                    .foregroundStyle(by: .value("Name", element.name))
+    
+            HStack(alignment: .center, spacing: 5) {
+                Button(action: {
+                    // Button action code here
+                    print("View Recommendation Button Tapped")
+                }) {
+                    HStack(spacing: 10) {
+                        Image(systemName: "dumbbell.fill")
+                            .resizable()
+                            .frame(width: 15, height: 15) // Set the size of the health icon
+                            .foregroundColor(.white) // Set the color of the health icon
+                        
+                        Text("Gym")
+                            .foregroundColor(.white) // Set the text color to white
+                            .font(.headline) // Set the font style for the label
+                    }.frame(width:70,height:10)
+                
+                    .padding()
+                    .background(Color.blue) // Set the background color of the button
+                    .cornerRadius(16) // Set the corner radius of the button
                 }
-                .frame(height: 300)
-                .chartXAxis(.hidden)
-                .chartLegend(position: .bottom, alignment: .center, spacing: 7)
-                VStack {
-                    Text("98%")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                    Text("of days have data")
-                        .font(.subheadline)
-                        .foregroundStyle(.gray)
+                Button(action: {
+                    // Button action code here
+//                    print("View Recommendation Button Tapped")
+                    
+                }) {
+                    HStack() {
+                        Image(systemName: "figure.pilates")
+                            .resizable()
+                            .frame(width: 20, height: 15) // Set the size of the health icon
+                            .foregroundColor(.white) // Set the color of the health icon
+                        
+                        Text("Pilates")
+                            .foregroundColor(.white) // Set the text color to white
+                            .font(.headline) // Set the font style for the label
+                    }.frame(width:90,height:10)
+                
+                    .padding()
+                    .background(Color.blue) // Set the background color of the button
+                    .cornerRadius(16) // Set the corner radius of the button
+                }
+                Button(action: {
+                    // Button action code here
+                    print("View Recommendation Button Tapped")
+                }) {
+                    HStack() {
+                        Image(systemName: "figure.run")
+                            .resizable()
+                            .frame(width: 15, height: 15) // Set the size of the health icon
+                            .foregroundColor(.white) // Set the color of the health icon
+                        
+                        Text("Running")
+                            .foregroundColor(.white) // Set the text color to white
+                            .font(.headline) // Set the font style for the label
+                    }.frame(width:90,height:10)
+                
+                    .padding()
+                    .background(Color.blue) // Set the background color of the button
+                    .cornerRadius(16) // Set the corner radius of the button
                 }
             }
+//            Text("Your experiment recommendation")
+//                .font(.title2)
+//                .fontWeight(.semibold)
+//            
+            let header = Text("Based on your historical data and preferences, we recommend changing your ") + Text("gym (cause)").foregroundColor(Color.black).bold() + Text(" to observe impacts on your ") + Text("step count (effect)").foregroundColor(Color.black).bold() + Text(".")
+//            header.font(.subheadline)
+//                .multilineTextAlignment(.center)
+            VStack(alignment: .center, spacing: 10) {
+                Text("Your experiment recommendation 🔬")
+                    .font(.headline)
+                    .foregroundColor(.black)
+                header.font(.subheadline).foregroundColor(.black).multilineTextAlignment(.center)
+//                Button(action: {
+//                    // Proceed action here
+//                }) {
+//                    Text("Proceed >")
+
+//                }
+//                HStack {
+//                    Spacer()
+//                    Button(action: {
+//                        // Proceed action here
+//                    }) {
+//                        Text("Proceed >")
+//                            .font(.footnote)
+//                            .foregroundColor(.black)
+//                            .underline() // Add underline to the text
+//                    }
+//                }
+            }
             .padding()
+            .background(Color.orange.opacity(0.5))
+            .cornerRadius(12)
+            .frame(width:350, height:150)
+            
+
+          
 //            Text("Missing Data Streaks")
 //                .font(.title2)
 //                .fontWeight(.semibold)
@@ -120,59 +290,11 @@ struct ExperimentRecommendation: View {
 //            Text("Patterns of no data collected")
 //                .font(.footnote)
 //                .foregroundStyle(.gray)
-//            Text("Daily Averages of Standing & Active Minutes")
-//                .font(.title2)
-//                .fontWeight(.semibold)
-//            
-//            Text("Daily average across 18 months")
-//                .font(.footnote)
-//                .foregroundStyle(.gray)
-//            LocationsChart()
+            StepsChart()
+            LocationsChart()
             
             
-//            Chart {
-//                ForEach(averageMins) { series in
-//                    ForEach(series.mins, id: \.day) { element in
-//                        LineMark(
-//                            x: .value("Day", element.day, unit: .day),
-//                            y: .value("Sales", element.mins)
-//                        )
-//                    }
-//                    .foregroundStyle(by: .value("Type", series.type))
-//                    .symbol(by: .value("Type", series.type))
-//                    .interpolationMethod(.catmullRom)
-//                }
-//            }
-            //            HStack(spacing: 20) {
-            //                VStack {
-            //                    Text("Total Days of Data")
-            //                        .font(.title3)
-            //                        .foregroundColor(.blue)
-            //                    Text("629")
-            //                        .font(.headline)
-            //                }
-            //
-            //                VStack {
-            //                    Text("Missing Days")
-            //                        .font(.title3)
-            //                        .foregroundColor(.blue)
-            //                    Text("11")
-            //                        .font(.headline)
-            //                }
-            //
-            //            }
-            
-            
-            //            GeometryReader { geometry in
-            //                Chart(adherenceDays) { element in
-            //                    BarMark(
-            //                        x: .value("Name", element.name),
-            //                        y: .value("Missing Days", element.days)
-            //                    )
-            //                }
-            //                .frame(width: geometry.size.width, height: 250) // Set the chart height here
-            //            }.padding()
-            //        }
+
         }.padding()
     }
 }
